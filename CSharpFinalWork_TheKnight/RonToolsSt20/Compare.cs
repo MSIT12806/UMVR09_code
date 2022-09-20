39013b745d2a640563b8f4e5fbe6bd3c78bf3b51
Robin <alegalone@gmail.com>
2022-09-20 08:36:27 +0800

    0920

---
+++ CSharpFinalWork_TheKnight/RonToolsSt20/Compare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RonToolsSt20
+{
+    public static class Compare
+    {
+        public static T Best<T>(Func<T,T, T> CompareFunc, params T[] objs)
+        {
+            T result = objs[0];
+            for (int i = 1; i < objs.Length; i++)
+            {
+                result = CompareFunc(objs[i], result);
+            }
+            return result;
+        }
+    }
+}
---
+++ CSharpFinalWork_TheKnight/RonToolsSt20/Singalton.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpFinalWork_TheKnight
+{
+    public class Singalton<T>
+    {
+        private static T _single;
+        private static object syncRoot = new object();
+
+        public static T SingletonObj
+        {
+            get
+            {
+                lock(syncRoot)
+                {
+                    if (_single == null)
+                    {
+                        _single = System.Activator.CreateInstance<T>();
+                    }
+                    return _single;
+                }
+            }
+        }
+    }
+}
