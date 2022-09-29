448d35279383b3c808c0b20aad1446c6994e74fe
robin <alegalone@gmail.com>
2022-09-29 12:17:21 +0800

    RonUnity

---
+++ RonUnity/RonUnity/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace RonUnity
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+        string gameVersion = "1";
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Debug.LogErrorFormat(gameObject,
+                "Multiple instances of {0} is not allow", GetType().Name);
+                DestroyImmediate(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+}
