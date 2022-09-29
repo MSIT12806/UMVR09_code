db51abb8bd1c0d308022dbd6dadeffaa9fd9b83a
robin <alegalone@gmail.com>
2022-09-29 21:28:35 +0800

    heap tree 等等.

--- RonTools/ConsoleApp1/Program.cs
+++ RonTools/ConsoleApp1/Program.cs
@@ -9,14 +9,148 @@ using System.Xml.Linq;
 
 namespace ConsoleApp1
 {
+    public class Coordinate
+    {
+        public int X; public int Y;
 
+        public Coordinate(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
 
+        public float GetDistance(Coordinate point)
+        {
+            var dX = point.X - this.X;
+            var dY = point.Y - this.Y;
+            var distanceSquare = (float)Math.Pow(dX, 2) + (float)Math.Pow(dY, 2);
+            return (float)Math.Pow(distanceSquare, 0.5);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(Coordinate))
+                return base.Equals(obj);
+            else
+                return ((Coordinate)obj).X == this.X && ((Coordinate)obj).Y == this.Y;
+        }
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+    public class AStarNode<T>
+    {
+        public T Location;
+        public T Parent;
+        public float CostFromStart;
+        public float CostToGoal;
+        public float TotalCost;
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            MinimaxSearchCase();
+            // MinimaxSearchCase();
+            Console.WriteLine(GetP(0));
+            Console.WriteLine(GetP(1));
+            Console.WriteLine(GetP(2));
+            Console.WriteLine(GetP(3));
+            Console.WriteLine(GetP(4));
+            Console.WriteLine(GetP(5));
+            Console.WriteLine(GetP(6));
+            int GetP(int tail)
+            {
+                return (int)((tail + 1) / 2) - 1;
+
+            }
         }
+        static void AStarSearchCase()
+        {
+            //set issue.
+            var Obstacle = new Coordinate[16]
+            {
+                new Coordinate(7,1),
+                new Coordinate(7,2),
+                new Coordinate(7,3),
+                new Coordinate(7,4),
+                new Coordinate(7,5),
+                new Coordinate(6,2),
+                new Coordinate(6,3),
+                new Coordinate(6,4),
+                new Coordinate(6,5),
+                new Coordinate(5,2),
+                new Coordinate(5,3),
+                new Coordinate(5,4),
+                new Coordinate(4,3),
+                new Coordinate(4,4),
+                new Coordinate(3,3),
+                new Coordinate(3,4),
+            };
+            var starPoint = new Coordinate(2, 3);
+            var goal = new Coordinate(6, 6);
+            Func<Coordinate, bool> canGo = (p) => !Obstacle.Contains(p);
+            Func<Coordinate, float> calculateWeight = (p) => p.GetDistance(goal);
+            var st = AStarSearch(starPoint, goal, canGo, calculateWeight);
+
+        }
+
+        private static object AStarSearch(Coordinate starPoint, Coordinate goal, Func<Coordinate, bool> canGo, Func<Coordinate, float> calculateWeight)
+        {
+            /*
+ AStarSearch( location StartLoc, location GoalLoc, agenttype Agent)
+{
+clear Open & Closed
+// initialize a start node
+StartNode.Loc = StartLoc;
+StartNode.CostFromStart = 0;
+StartNode.CostToGoal = PathCostEstimate(StartLoc, GoalLoc, Agent);
+StartNode.TotalCost = StartNode.CostToGoal ;
+StartNode.Parent = NULL;
+push StartNode on Open;
+
+// process the list until success or failure
+while Open is not empty {
+pop Node from Open // Node has the lowest TotalCost... TRY HEAP TREE!!
+
+ // if at a goal, we’re done
+if (Node is a goal node) {
+construct a path backward from Node to StartLoc
+return SUCCESS;
+}
+else {
+for each successor NewNode of Node {
+NewCost = Node.CostFromStart + TraverseCost(Node, NewNode, Agent);
+
+// ignore this node if exists and no improvement
+if (NewNode is in Open or Closed) and
+(NewNode.CostFromStart <= NewCost) {
+continue;
+}
+else { // store the new or improved information
+NewNode.Parent = Node;
+NewNode.CostFromStart = NewCost;
+NewNode.CostToGoal = PathCostEstimate(NewNode.Loc, GoalLoc, Agent);
+NewNode.TotalCost = NewNode.CostFromStart + NewNode.CostToGoal;
+if (NewNode is in Closed) {
+remove NewNode from Closed
+}
+if (NewNode is in Open) {
+adjust NewNode’s position in Open
+}
+else {
+Push NewNode onto Open
+}
+}
+}
+}
+push Node onto Closed
+}
+}
+             */
+            throw new NotImplementedException();
+        }
+
         static void DepthFirstSearchCase()
         {
             //測試樹狀結構
--- RonTools/RonTools/DataStruct/RonTreeNode.cs
+++ RonTools/RonTools/DataStruct/RonTreeNode.cs
@@ -6,7 +6,7 @@ namespace RonTools.DataStruct
 {
     public class RonTreeNode<T>
     {
-        public readonly RonTreeNode<T> Parent;
+        public RonTreeNode<T> Parent;
         public readonly List<RonTreeNode<T>> Children = new List<RonTreeNode<T>>();
         public readonly T Value;
         public RonTreeNode(T v) { Value = v; }
@@ -21,5 +21,86 @@ namespace RonTools.DataStruct
         {
             return Value.ToString();
         }
+    }
+    public class RonBinraryTreeNode<T>
+    {
+        public readonly T Value;
+        public RonBinraryTreeNode<T> Parent;
+        public RonBinraryTreeNode<T> Left;
+        public RonBinraryTreeNode<T> Right;
+        public RonBinraryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public bool IsFull { get { return Left == null || Right == null; } }
+    }
+    /*
+     * 一個堆積樹必定為完整二元樹(complete binary tree), 且通常會用陣列來實作.
+     */
+
+    /*
+     * 加入一個元素(12)
+     * 首先, 新加入的元素會被放到最後的葉節點
+     * 然後新加入的節點會跟父節點做比對, 若小於父節點則往上升, 直至滿足堆積樹的條件為止才停下來
+     * 堆積樹取極值是O(1)，加入 & 取出 O(logN)
+     */
+    public class RonNode<T>
+    {
+        public readonly T Value;
+        public RonNode(T v)
+        {
+            Value = v;
+        }
+    }
+    public class RonHeapTree<T> where T : IComparable
+    {
+        private List<RonNode<T>> _tree = new List<RonNode<T>>();
+        public int Tail { get { return _tree.Count - 1; } }
+        public RonBinraryTreeNode<T> Head;
+        public int Add(T newValue)
+        {
+            RonNode<T> newNode = new RonNode<T>(newValue);
+            _tree.Add(newNode);
+            int doSwapIdx = Tail;
+            while (true)
+            {
+                if (!Swap(doSwapIdx, out doSwapIdx))
+                {
+                    break;
+                }
+            }            
+            return Tail;
+        }
+        bool Swap(int nowNodeIndex, out int pi)
+        {
+            var nowNode = _tree[nowNodeIndex];
+            (int _pi, RonNode<T> p) = GetParent(nowNodeIndex);
+            pi = _pi;
+            if(p == null) return false;
+            int r = nowNode.Value.CompareTo(p.Value);
+            if (r > 0)
+            {
+                _tree[pi] = _tree[nowNodeIndex];
+                _tree[nowNodeIndex] = p;
+                return true;
+            }
+            return false;
+                
+        }
+        public (int index, RonNode<T> item) GetParent(int nodeIndex)
+        {
+
+            if (nodeIndex > 0)
+            {
+                int i = (int)((Tail + 1) / 2) - 1;
+                return (i, _tree[i]);
+            }
+            else
+            {
+                return (-1, null);
+            }
+        }
+
     }
 }
