81e68c803c2bca82e4b34f83e1bf0bd547dc344d
robin <alegalone@gmail.com>
2022-09-29 15:02:16 +0800

    嘗試將獨體模式擴展為泛用型別

--- RonUnity/RonUnity/GameManager.cs
+++ RonUnity/RonUnity/GameManager.cs
@@ -3,21 +3,34 @@ using UnityEngine;
 
 namespace RonUnity
 {
-    public class GameManager : MonoBehaviour
+    public class Singleton<T> where T : MonoBehaviour
     {
-        public static GameManager instance;
+        /*
+         * 獨體模式：
+         * 讓注入的類別(gameobject)，只能單一。
+         * 應用static
+         */
+        /*
+         * 自定義組件，又名 MonoBehaviour 派生對象，其構造函數中不能有參數，也不能使用 new 手動創建。組件是策略設計模式的一部分，只能使用 AddComponent() 創建
+         */
+        public static T instance;
         string gameVersion = "1";
-        void Awake()
+        //void Awake()
+        //{
+        //    MustOnlyOne();
+        //}
+
+        public void MustOnlyOne()
         {
             if (instance != null)
             {
-                Debug.LogErrorFormat(gameObject,
+                Debug.LogErrorFormat(instance.gameObject,
                 "Multiple instances of {0} is not allow", GetType().Name);
-                DestroyImmediate(gameObject);
+                UnityEngine.Object.DestroyImmediate(instance.gameObject);
                 return;
             }
-            DontDestroyOnLoad(gameObject);
-            instance = this;
+            UnityEngine.Object.DontDestroyOnLoad(instance.gameObject);
+            //instance = this;
         }
     }
 }
