864d2f4706d4cb4943276263f5c831fc98f89025
robin <alegalone@gmail.com>
2022-10-12 21:16:31 +0800

    some new tools

---
+++ RonTools/RonReflectionTools/RonReflectiveExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RonReflectionTools
+{
+    public static class RonReflectiveExtension
+    {
+        static RonReflectiveExtension() { }
+        /// <summary>
+        /// 創建所有衍伸實例
+        /// </summary>
+        public static IEnumerable<T> InstanceDrivedTypes<T>(params object[] constructorArgs) where T : class, IComparable<T>
+        {
+            List<T> objects = new List<T>();
+            foreach (Type type in
+                Assembly.GetAssembly(typeof(T)).GetTypes()
+                .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T))))
+            {
+                objects.Add((T)Activator.CreateInstance(type, constructorArgs));
+            }
+            objects.Sort();
+            return objects;
+        }
+    }
+}
